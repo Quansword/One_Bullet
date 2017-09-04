d2643137e113d38cc2397fb3ebac9df038a66116
Sean Boland <Quanbook@gmail.com>
2017-09-04 15:59:06 -0600

    Added Level class

---
+++ OneBullet/OneBullet/Level.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace OneBullet
+{
+	class Level
+	{
+		public void Initialize()
+		{
+
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+
+		}
+	}
+}
