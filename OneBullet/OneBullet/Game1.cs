66fe043fef00dfa9f9170ac734c62b2f1a330878
Sean Boland <Quanbook@gmail.com>
2017-09-07 08:16:14 -0600

    Changed back to original testing resolution

--- OneBullet/OneBullet/Game1.cs
+++ OneBullet/OneBullet/Game1.cs
@@ -23,9 +23,9 @@ namespace OneBullet
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
 
-			graphics.PreferredBackBufferWidth = 3840;
-			graphics.PreferredBackBufferHeight = 2160;
-			graphics.IsFullScreen = true;
+			graphics.PreferredBackBufferWidth = 1280;
+			graphics.PreferredBackBufferHeight = 720;
+			graphics.IsFullScreen = false;
 			graphics.ApplyChanges();
 
 			charHeight = GraphicsDevice.Viewport.Height / 7;
